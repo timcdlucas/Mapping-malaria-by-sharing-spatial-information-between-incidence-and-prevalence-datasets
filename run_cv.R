73c3a770ced925ac0bb40f5eff3d8108179ac444
Tim Lucas <timcdlucas@gmail.com>
2018-06-12 19:41:42 +0100

    Small edits, add delays to cv.

--- run_cv.R
+++ run_cv.R
@@ -1,13 +1,15 @@
 
 
-run_cv <- function(cv_data, mesh, model.args){
+run_cv <- function(cv_data, mesh, model.args, parallel_delay = 60){
 
   stopifnot(inherits(cv_data, 'ppj_cv'))
 
   for(i in seq_along(cv_data)){
-    model[[i]] <- fit_model(cv_data[[i]]$train, mesh, model.args)
+
+    Sys.sleep(runif(0, 60, 1))
+    models[[i]] <- fit_model(cv_data[[i]]$train, mesh, model.args)
     results[[i]] <- cv_performance(moodel[[i]], cv_data[[i]]$test)
   }
 
-  return(list(model, results)
+  return(list(summary = summary, models = models, results = results))
 }
