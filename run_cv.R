9fd057485e1e6476ddcce956952eb1f4cc2c9251
Tim Lucas <timcdlucas@gmail.com>
2018-06-22 16:19:36 +0100

    First pass at function to run multiple models.

--- run_cv.R
+++ run_cv.R
@@ -4,12 +4,49 @@ run_cv <- function(cv_data, mesh, model.args, parallel_delay = 60){
 
   stopifnot(inherits(cv_data, 'ppj_cv'))
 
+  models <- list()
+  results <- list()
+  
   for(i in seq_along(cv_data)){
-
-    Sys.sleep(runif(0, 60, 1))
+    Sys.sleep(runif(1, 0, parallel_delay))
     models[[i]] <- fit_model(cv_data[[i]]$train, mesh, model.args)
-    results[[i]] <- cv_performance(moodel[[i]], cv_data[[i]]$test)
+    results[[i]] <- cv_performance(models[[i]]$predictions, cv_data[[i]]$test)
   }
+  
+  summary <- summarise_cv_results(results)
 
   return(list(summary = summary, models = models, results = results))
 }
+
+
+
+
+
+summarise_cv_results <- function(results){
+  
+  combined_aggregated <- 
+    lapply(seq_along(results), function(x) cbind(results[[x]]$polygon_pred_obs, fold = x)) %>% 
+      do.call(rbind, .)
+  
+  polygon_metrics <- combined_aggregated %>% 
+    summarise(RMSE = sqrt(mean((pred_api - response) ^ 2)),
+              MAE = mean(abs(pred_api - response)),
+              pearson = cor(pred_api, response, method = 'pearson'),
+              spearman = cor(pred_api, response, method = 'spearman'),
+              RMSE_cases = sqrt(mean((incidence_count - pred_incidence_count) ^ 2)),
+              MAE_cases = mean(abs(incidence_count - pred_incidence_count)),
+              total_cases = sum(incidence_count - pred_incidence_count))
+  
+  combined_summaries <- 
+    lapply(seq_along(results), function(x) cbind(results[[x]]$polygon_metrics, fold = x)) %>% 
+    do.call(rbind, .)
+  
+  return(list(combined_aggregated = combined_aggregated,
+              combined_summaries = combined_summaries,
+              polygon_metrics = polygon_metrics))
+  
+}
+
+
+
+
