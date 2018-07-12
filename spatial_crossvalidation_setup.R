47f3a779dd965c227cbdcee1449efe1ed0a10144
Tim Lucas <timcdlucas@gmail.com>
2018-07-12 14:47:09 +0100

    Start doing spaitla cv. Treat polyongs as points.

--- spatial_crossvalidation_setup.R
+++ spatial_crossvalidation_setup.R
@@ -1,1 +1,86 @@
-# Create spatial cross validation folds
+
+
+#'@return a 'ppj_cv' object. Which is a list of paired train/test 'ppf_data' objects
+#'
+
+cv_spatial_folds <- function(data, k = 5){
+  
+  # Polygons to points
+  centroids <- sapply(seq_len(nrow(data$shapefiles)), 
+                      function(x) rgeos::gCentroid(data$shapefiles[x, ])@coords)
+  
+  centroids <- t(centroids)
+  colnames(centroids) <- c('longitude', 'latitude')
+  
+  all_coords <- rbind(centroids, data$pr[, c('longitude', 'latitude')])
+  
+  # Do k means
+  
+  
+  
+  # reindex polygons.
+  
+  
+  
+  
+  
+  # fold is length nrow(polygons), foldspr is length nrow(points)
+  #   Should be values 1 to k.
+  folds <- f[s]
+  foldspr <- fpr[spr]
+  
+  data_cv <- list()
+  class(data_cv) <- c('ppj_cv', 'list')
+  
+  for(i in 1:k){
+    data_cv[[i]] <- subset_data_cv(data, folds, foldspr, i)
+  }
+  
+  return(data_cv)
+  
+}
+
+
+# 
+# subset_data_cv <- function(data, polygon_folds, pr_folds, k){
+#   
+#   data_fold <- list(train = list(), test = list())
+#   
+#   data_fold$train$pr <- data$pr[pr_folds != k, ]
+#   data_fold$train$pr_covs <- data$pr_covs[pr_folds != k, ]
+#   
+#   data_fold$train$polygon <- data$polygon[polygon_folds != k, ]
+#   cov_rows <- data$covs$area_id %in% data_fold$train$polygon$shapefile_id
+#   data_fold$train$covs <- data$covs[cov_rows, ]
+#   data_fold$train$pop <- data$pop[cov_rows]
+#   data_fold$train$coords <- data$coords[cov_rows, ]
+#   data_fold$train$cov_rasters <- data$cov_rasters
+#   data_fold$train$pop_raster <- data$pop_raster
+#   data_fold$train$shapefiles <- data$shapefiles[data$shapefiles$area_id %in% data_fold$train$covs$area_id, ]
+#   
+#   
+#   data_fold$test$pr <- data$pr[pr_folds == k, ]
+#   data_fold$test$pr_covs <- data$pr_covs[pr_folds == k, ] # this shouldn't ever be used as far as I can see.
+#   
+#   data_fold$test$polygon <- data$polygon[polygon_folds == k, ]
+#   data_fold$test$covs <- data$covs[!cov_rows, ]
+#   data_fold$test$pop <- data$pop[!cov_rows]
+#   data_fold$test$coords <- data$coords[!cov_rows, ]
+#   data_fold$test$cov_rasters <- data$cov_rasters
+#   data_fold$test$pop_raster <- data$pop_raster
+#   data_fold$test$shapefiles <- data$shapefiles[!(data$shapefiles$area_id %in% data_fold$train$covs$area_id), ]
+#   
+#   class(data_fold$test) <- 'ppj_data'
+#   class(data_fold$train) <- 'ppj_data'
+#   
+#   
+#   return(data_fold)
+#   
+# }
+# 
+# 
+# 
+# 
+
+
+
