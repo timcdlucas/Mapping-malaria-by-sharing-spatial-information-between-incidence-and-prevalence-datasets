be4349a26176602e14c9c572742aa43b54d184d4
Tim Lucas <timcdlucas@gmail.com>
2018-08-30 09:43:18 +0100

    Get madagascar more up and running.

--- run_analysis_madagascar.R
+++ run_analysis_madagascar.R
@@ -1,6 +1,96 @@
 
+########
+# master script for point Vs polygon Vs joint analysis
+# Tim Lucas
+# 2018-05-30
+###########
 
 
+# setwd('~/Dropbox/work/analysis/point_polygon_joint_comparison')
+# setwd('~/timz/timothy/point_polygon_joint_comparison')
+
+# define paths
+
+PR_path <- '~/timz/GBD2017/Processing/Stages/04b_PR_DB_Import_Export/Verified_Outputs/2018_02_15/pfpr_dump.csv'
+API_path <- '~/timz/GBD2017/Processing/Stages/04c_API_Data_Export/Checkpoint_Outputs/subnational.csv'
+pop_path <- '~/timz/GBD2017/Processing/Stages/03_Muster_Population_Figures/Verified_Outputs/Output_Pop_At_Risk_Pf_5K/ihme_corrected_frankenpop_All_Ages_3_2012_at_risk_pf.tif'
+shapefile_path = '~/timz/master_geometries/Admin_Units/Global/GBD/GBD2017_MAP/GBD2017_MAP_MG_5K/'
+
+cov_raster_paths <- c(
+  '~/timz/mastergrids/MODIS_Global/MOD11A2_LST/LST_Day/5km/Synoptic/LST_Day.Synoptic.Overall.mean.5km.mean.tif',
+  '~/timz/mastergrids/MODIS_Global/MCD43B4_BRDF_Reflectance/EVI/5km/Synoptic/EVI.Synoptic.Overall.mean.5km.mean.tif',
+  '~/timz/mastergrids/Other_Global_Covariates/TemperatureSuitability/TSI_Pf_Dynamic/5km/Synoptic/TSI-Martens2-Pf.Synoptic.Overall.Mean.5km.Data.tif',
+  '~/timz/GBD2017/Processing/Static_Covariates/MAP/other_rasters/accessibility/accessibility.5k.MEAN.tif',
+  '~/timz/mastergrids/Other_Global_Covariates/Elevation/SRTM-Elevation/5km/Synoptic/SRTM_elevation.Synoptic.Overall.Data.5km.mean.tif',
+  '~/timz/mastergrids/MODIS_Global/MOD11A2_LST/LST_Day/5km/Synoptic/LST_Day.Synoptic.Overall.SD.5km.mean.tif',
+  #'~/timz/mastergrids/MODIS_Global/MCD43B4_BRDF_Reflectance/TCB/5km/Synoptic/TCB.Synoptic.Overall.mean.5km.mean.tif',
+  '~/timz/mastergrids/Other_Global_Covariates/NightTimeLights/VIIRS_DNB_Monthly/5km/Annual/VIIRS-SLC.2016.Annual.5km.MEDIAN.tif',
+  #'~/timz/mastergrids/Other_Global_Covariates/UrbanAreas/Global_Urban_Footprint/From_86m/5km/Global_Urban_Footprint_5km_PropUrban.tif',
+  '~/timz/mastergrids/MODIS_Global/MCD43B4_BRDF_Reflectance/TCW/5km/Synoptic/TCW.Synoptic.Overall.mean.5km.mean.tif'
+)
+
+# load packages
+
+## Spatial packages
+library(raster)
+library(maptools)
+library(rgeos)
+
+## dataframe packages
+library(dplyr)
+library(readr)
+library(magrittr)
+library(tidyr)
+
+## For standardising prevalence
+library(malariaAtlas)
+
+## For inc prevalence conversions
+library(GBDutils)
+
+## Plotting packages
+library(ggplot2)
+library(cowplot)
+theme_set(theme_minimal())
+
+##  Modelling packages
+library(TMB)
+#library(stantmb)
+library(INLA)
+if(Sys.info()["sysname"] != 'Windows'){
+  message('using INLA unix workaround')
+  INLA:::inla.dynload.workaround()
+} else {
+  message('Not using INLA unix workaround. Expect you are using winows.')
+}
+library(INLAutils)
+library(sparseMVN)
+
+
+# Parallel processing
+library(foreach)
+library(doParallel)
+cl <- makeCluster(min(detectCores() - 1, 20))
+registerDoParallel(cl)
+
+
+# load functions
+
+source('collect_data.R')
+source('process_data.R')
+source('CombineRasters.R')
+source('parallel-raster-extract.R')
+source('build_inla_meshes.R')
+source('fit_model.R')
+source('run_cv.R')
+source('random_crossvalidation_setup.R')
+source('spatial_crossvalidation_setup.R')
+source('plotting_functions.R')
+
+# Compile the model
+compile("joint_model.cpp")
+
+set.seed(180530)
 
 # load all data
 # Perhaps read all raster years and have a step in process data to choose the right one? Or something. Kinda annoying.
@@ -11,9 +101,9 @@ data <- load_data(PR_path,
                   shapefile_path, 
                   shapefile_pattern = '.shp$', 
                   useiso3 = 'MDG', 
-                  admin_unit_level = 'ADMIN1',
-                  pr_year = 2013,
-                  api_year = 2013)
+                  admin_unit_level = 'ADMIN3',
+                  pr_year = 2016,
+                  api_year = 2015)
 
 
 # madagascar
