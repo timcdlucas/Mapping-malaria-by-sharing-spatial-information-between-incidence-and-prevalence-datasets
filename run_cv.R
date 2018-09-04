052bfed9e318ade35933818346ad2b808acf8a6b
Tim Lucas <timcdlucas@gmail.com>
2018-09-04 15:34:49 +0100

    Export correct scope objects for windows parallel run.

--- run_cv.R
+++ run_cv.R
@@ -20,7 +20,7 @@ run_cv <- function(cv_data, mesh, its = 10, model.args = NULL, CI = 0.95, parall
   } else {
     cl <- makePSOCKcluster(cores, outfile = "")
     setDefaultCluster(cl)
-    clusterExport(cl, c('fit_model', 'data_idn', 'mesh_idn', 'arg_list', 'predict_model', 'predict_uncertainty', 'iidDraw',
+    clusterExport(cl, c('fit_model', 'cv_data', 'mesh', 'model.args', 'predict_model', 'predict_uncertainty', 'iidDraw',
                         'make_startend_index', 'MakeField', 'PrevIncConversion', 'extractFieldProperties', 'makeLinearPredictor'))
     clusterEvalQ(NULL, library(magrittr))
     clusterEvalQ(NULL, library(INLA))
