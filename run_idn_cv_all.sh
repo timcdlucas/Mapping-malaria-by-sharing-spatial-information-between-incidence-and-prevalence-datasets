d3a64b0a4132f3a2bb2478b765ff3faa26fea76c
Anita <anita.nandi@bdi.ox.ac.uk>
2018-09-24 12:59:37 +0100

    Added bash script to run all cv models at once

---
+++ run_idn_cv_all.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+declare -a cv_type=("random" "spatial")
+declare -a model_type=("points" "polygons" "joint")
+
+for i in "${cv_type[@]}" 
+do
+   for j in "${model_type[@]}" 
+   do
+      echo "Run $i $j"
+      ./run_idn_cv_single.sh $i $j
+   done
+done
+
