3b95d83cea27c82187692d15f7c342e28b2d9c0c
Tim Lucas <timcdlucas@gmail.com>
2019-05-22 21:02:58 +0100

    Apparnetly need country name for one coutnry pr now.

--- run_analysis_sen.R
+++ run_analysis_sen.R
@@ -110,7 +110,7 @@ data <- load_data(PR_path,
                   shapefile_path, 
                   shapefile_pattern = '.shp$', 
                   useiso3 = 'SEN', 
-                  pr_country = NULL,
+                  pr_country = 'Senegal',
                   admin_unit_level = 'ADMIN2', # todo
                   pr_year = c(2007:2011),
                   api_year = 2009)
